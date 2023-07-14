91278a5670f58c05b83c29b232d15db629240ae6
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-07-14 10:12:13 -0300

    feat(basic fluxogram)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import DashBoard from "./containers/Dashboard";
 import CreateFluxogram from "./pages/createFluxogram";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DnDFlow from "./pages/fluxogram/DnDFlow";
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <DashBoard>
         <Routes>
-          <Route path="/create-fluxogram" element={<CreateFluxogram />} />
-          <Route path="/fluxogram" element={<DnDFlow />} />
+          <Route path="/" element={<CreateFluxogram />} />
+
         </Routes>
 
       </DashBoard>
--- src/pages/createFluxogram/index.jsx
+++ src/pages/createFluxogram/index.jsx
@@ -1,65 +1,93 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useCallback } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import ReactFlow, {
+  ReactFlowProvider,
   addEdge,
-  MiniMap,
-  Controls,
-  Background,
   useNodesState,
   useEdgesState,
-  Panel,
+  Controls,
 } from 'reactflow';
-
-import { nodes as initialNodes, edges as initialEdges } from './initial-elements';
-import CustomNode from './CustomNode';
-
 import 'reactflow/dist/style.css';
-import './overview.css';
 
-const nodeTypes = {
-  custom: CustomNode,
-};
+import Sidebar from './Sidebar';
 
-const minimapStyle = {
-  height: 120,
-};
+import './index.css';
 
-const onInit = (reactFlowInstance) => console.log('flow loaded:', reactFlowInstance);
+const initialNodes = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'input node' },
+    position: { x: 250, y: 5 },
+  },
+];
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
 
 const CreateFluxogram = () => {
-  // eslint-disable-next-line no-unused-vars
+  const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
 
-  const edgesWithUpdatedTypes = edges.map((edge) => {
-    if (edge.sourceHandle) {
-      const edgeType = nodes.find((node) => node.type === 'custom').data.selects[edge.sourceHandle];
-      edge.type = edgeType;
-    }
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
 
-    return edge;
-  });
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+      const type = event.dataTransfer.getData('application/reactflow');
+
+      // check if the dropped element is valid
+      if (typeof type === 'undefined' || !type) {
+        return;
+      }
+
+      const position = reactFlowInstance.project({
+        x: event.clientX - reactFlowBounds.left,
+        y: event.clientY - reactFlowBounds.top,
+      });
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance]
+  );
+
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edgesWithUpdatedTypes}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      onInit={onInit}
-      fitView
-      attributionPosition="top-right"
-      nodeTypes={nodeTypes}
-    >
-      <MiniMap style={minimapStyle} zoomable pannable />
-      <Controls />
-      <Background style={{ background: '#737373' }} color="#aaa" gap={16} />
-      <Panel style={{ width: '20%', height: '70%', background: '#f2f2f2', borderRadius: 8 }} position='top-left'>
-
-      </Panel>
-    </ReactFlow>
+    <div className="dndflow">
+      <Sidebar />
+      <ReactFlowProvider>
+        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={setReactFlowInstance}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            fitView
+          >
+            <Controls />
+          </ReactFlow>
+        </div>
+      </ReactFlowProvider>
+    </div>
   );
 };
 
