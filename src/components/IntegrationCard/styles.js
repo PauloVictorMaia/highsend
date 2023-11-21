b52339901503f62f6dd55ce56e74128e69a6167b
Paulo <pauloaspinfa@gmail.com>
2023-11-21 12:39:10 -0300

    integrations screen

--- src/components/IntegrationCard/styles.js
+++ src/components/IntegrationCard/styles.js
@@ -2,13 +2,18 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 250px;
-  height: 350px;
+  height: 320px;
   border: 0.5px solid rgba(0,0,0,0.15);
   border-radius: 8px; 
   color: rgba(0,0,0,0.75);
   cursor: pointer;
   padding: 10px;
   box-sizing: border-box;
+  text-align: center;
+
+  span {
+    font-size: 21px;
+  }
 
   &:hover {
     box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
@@ -17,7 +22,6 @@ export const Container = styled.div`
 
 export const ImgContainer = styled.div`
   width: 100%;
-  height: 35%;
   padding: ${({padding}) => padding};
   box-sizing: border-box;
   border-radius: 8px 8px 0 0;
@@ -25,17 +29,18 @@ export const ImgContainer = styled.div`
 `;
 
 export const IntegrationImg = styled.img`
-  width: 100%;
+  width: 70px;
   height: auto;
 `;
 
 export const DescriptionContainer = styled.div`
   width: 100%;
-  height: 45%;
   padding: 5px;
   box-sizing: border-box;
-  font-size: 0.95rem;
+  font-size: 12px;
+  color: #666666;
   line-height: 1.3;
+  margin-bottom: 40px;
   /* background-color: yellow; */
 `;
 
@@ -46,9 +51,10 @@ export const IconContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-top: 30px;
   /* background-color: green; */
 
   >svg {
-    
+    font-size: 30px;
   }
 `;
