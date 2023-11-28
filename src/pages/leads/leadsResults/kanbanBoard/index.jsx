2bfeb9c368e23bd9e15e286188578f3de6d4d1bb
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-11-28 09:50:15 -0300

    feat kanban board

--- src/pages/leads/leadsResults/kanbanBoard/index.jsx
+++ src/pages/leads/leadsResults/kanbanBoard/index.jsx
@@ -1,30 +1,76 @@
 import LeadsContext from "../context";
 import { useContext } from "react";
-import { AddColumnButton, Board, ButtonContainer, Container } from "./styles";
+import { AddColumnButton, Board, ButtonContainer, Container, NewListButton, NewListContent, NewListInput, NewListInputContainer } from "./styles";
 import LeadsList from "../../../../components/LeadsList";
 import AddIcon from '@mui/icons-material/Add';
+import ClearIcon from '@mui/icons-material/Clear';
+import { useState, useRef } from "react";
+import { toast } from "react-toastify";
+import { v4 as uuidv4 } from 'uuid';
 
 function KanbanBoard() {
 
   const { leadsList, setLeadsList } = useContext(LeadsContext);
+  const [showNewListInput, setShowNewListInput] = useState(false);
+  const [newListName, setNewListName] = useState("");
+  const inputRef = useRef(null);
 
   function createNewColumn() {
+
+    if (!newListName) {
+      toast.warning("Defina um nome para a nova lista");
+      return;
+    }
+
     const newList = {
-      id: "123",
-      title: "Nova coluna",
+      id: uuidv4(),
+      title: newListName,
       cards: []
     }
 
-    setLeadsList(lists => [...lists, newList])
+    setLeadsList(lists => [...lists, newList]);
+    closeInput();
+  }
+
+  function closeInput() {
+    setShowNewListInput(false);
+    setNewListName("");
   }
 
   return (
     <Board>
       <Container>
         {leadsList.map(list => <LeadsList key={list.id} data={list} />)}
+
+        {showNewListInput &&
+          <NewListInputContainer>
+            <NewListContent>
+              <NewListInput
+                ref={inputRef}
+                label="Nova lista"
+                variant="outlined"
+                type={"text"}
+                name="list"
+                value={newListName}
+                onChange={(e) => setNewListName(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    createNewColumn();
+                  }
+                }}
+                autoFocus
+              />
+              <NewListButton onClick={() => createNewColumn()}>Adicionar</NewListButton>
+            </NewListContent>
+          </NewListInputContainer>
+        }
+
         <ButtonContainer>
-          <AddColumnButton onClick={() => createNewColumn()}>
-            <AddIcon />
+          <AddColumnButton
+            onClick={() => setShowNewListInput(!showNewListInput)}
+            background={showNewListInput}
+          >
+            {showNewListInput ? <ClearIcon /> : <AddIcon />}
           </AddColumnButton>
         </ButtonContainer>
       </Container>
--- src/pages/leads/leadsResults/kanbanBoard/styles.js
+++ src/pages/leads/leadsResults/kanbanBoard/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { TextField } from "@mui/material";
 
 export const Board = styled.div`
   width: 100%;
@@ -26,14 +27,68 @@ export const AddColumnButton = styled.button`
   border: none;
   outline: none;
   cursor: pointer;
-  background-color: #4339F2;
+  background-color: ${({ background }) => background ? "#ff4d4d" : "#4339F2"};
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-top: 13px;
 
   >svg {
     width: 100%;
     height: 100%;
     color: #fff;
   }
+`;
+
+export const NewListInputContainer = styled.div`
+  flex: 0 0 280px;
+`;
+
+export const NewListContent = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const NewListInput = styled(TextField)`
+  border: 1.5px solid red;
+  border: none;
+  width: 70%;
+
+  div {
+    border-radius: 8px;
+  }
+
+  label {
+    color: #526B92;
+  }
+
+  fieldset {
+    border: 2px solid #E0EAFF;
+  }
+
+  input:focus {
+    fieldset {
+      border: 2px solid #4339F2;
+    }
+
+    label {
+      color: #4339F2;
+    }
+  }
+`;
+
+export const NewListButton = styled.button`
+  width: 25%;
+  height: 35px;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background-color: #4339F2;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
